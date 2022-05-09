b8b11330ddff5302dcd96205fd32233c77950d38
Name Your <m20211000@novaims.unl.pt>
2022-05-09 16:13:57 +0100

    e

--- charles/charles.py
+++ charles/charles.py
@@ -1,45 +1,30 @@
 from random import shuffle, choice, sample, random
 from operator import attrgetter
 from copy import deepcopy
-
+import numpy as np
+import operator
+from charles.mutation import swap_mutation, sudoku_mutation
 
 class Individual:
-    def __init__(
-        self,
-        representation=None,
-        size=None,
-        replacement=True,
-        valid_set=None,
-    ):
-        if representation is None:
-            if replacement == True:
-                self.representation = [choice(valid_set) for i in range(size)]
-            elif replacement == False:
-                self.representation = sample(valid_set, size)
-        else:
-            self.representation = representation
-        self.fitness = self.get_fitness()
+    def __init__(self):
+        self.values = np.zeros((Nd, Nd))
+        self.fitness = None
 
-    def get_fitness(self):
+    def update_fitness(self):
         raise Exception("You need to monkey patch the fitness path.")
 
     def get_neighbours(self, func, **kwargs):
         raise Exception("You need to monkey patch the neighbourhood function.")
 
-    def index(self, value):
-        return self.representation.index(value)
-
-    def __len__(self):
-        return len(self.representation)
-
-    def __getitem__(self, position):
-        return self.representation[position]
-
-    def __setitem__(self, position, value):
-        self.representation[position] = value
-
     def __repr__(self):
-        return f"Individual(size={len(self.representation)}); Fitness: {self.fitness}"
+        return f"Individual({self.values}); Fitness: {self.fitness}"
+    
+    def mutate(self, mutation_rate, given,swap=True):#, mutation = swap_mutation):
+        
+        if swap:
+            return swap_mutation(self)
+        else:
+            return sudoku_mutation(self,mutation_rate,given)
 
 
 class Population:
@@ -106,3 +91,159 @@ class Population:
 
     def __repr__(self):
         return f"Population(size={len(self.individuals)}, individual_size={len(self.individuals[0])})"
+
+
+
+random.seed()
+
+Nd = 9 
+
+class Candidate(object): #not berfin
+    """ A candidate solutions to the Sudoku puzzle. """
+
+    def __init__(self):
+        self.values = np.zeros((Nd, Nd))
+        self.fitness = None
+        return
+
+    def update_fitness(self):
+        """ The fitness of a candidate solution is determined by how close it is to being the actual solution to the puzzle.
+        The actual solution (i.e. the 'fittest') is defined as a 9x9 grid of numbers in the range [1, 9]
+        where each row, column and 3x3 block contains the numbers [1, 9] without any duplicates (see e.g. http://www.sudoku.com/);
+        if there are any duplicates then the fitness will be lower. """
+
+        column_count = np.zeros(Nd)
+        block_count = np.zeros(Nd)
+        column_sum = 0
+        block_sum = 0
+
+        self.values = self.values.astype(int)
+        # For each column....
+        for j in range(0, Nd):
+            for i in range(0, Nd):
+                column_count[self.values[i][j] - 1] += 1
+
+            # unique
+            # column_sum += (1.0 / len(set(column_count))) / Nd
+            # set
+            # for k in range(len(column_count)):
+            #     if column_count[k] != 0:
+            #         column_sum += (1/Nd)/Nd
+            # duplicate
+            for k in range(len(column_count)):
+                if column_count[k] == 1:
+                    column_sum += (1/Nd)/Nd
+            column_count = np.zeros(Nd)
+
+        # For each block...
+        for i in range(0, Nd, 3):
+            for j in range(0, Nd, 3):
+                block_count[self.values[i][j] - 1] += 1
+                block_count[self.values[i][j + 1] - 1] += 1
+                block_count[self.values[i][j + 2] - 1] += 1
+
+                block_count[self.values[i + 1][j] - 1] += 1
+                block_count[self.values[i + 1][j + 1] - 1] += 1
+                block_count[self.values[i + 1][j + 2] - 1] += 1
+
+                block_count[self.values[i + 2][j] - 1] += 1
+                block_count[self.values[i + 2][j + 1] - 1] += 1
+                block_count[self.values[i + 2][j + 2] - 1] += 1
+
+                # unique
+                # block_sum += (1.0 / len(set(block_count))) / Nd
+                # set
+                # for k in range(len(block_count)):
+                #     if block_count[k] != 0:
+                #         block_sum += (1/Nd)/Nd
+                # duplicate
+                for k in range(len(block_count)):
+                    if block_count[k] == 1:
+                        block_sum += (1/Nd)/Nd
+                block_count = np.zeros(Nd)
+
+        # Calculate overall fitness.
+        if int(column_sum) == 1 and int(block_sum) == 1:
+            fitness = 1.0
+        else:
+            fitness = column_sum * block_sum
+
+        self.fitness = fitness
+        return
+
+    
+
+class Population(object): #not berfin
+    """ A set of individual solutions to the Sudoku puzzle.
+    These individuals are also known as the chromosomes in the population. """
+
+    def __init__(self):
+        self.individuals = []
+        return
+
+    def seed(self, Nc, given):
+        self.individuals = []
+
+        # Determine the legal values that each square can take.
+        helper = Individual()
+        helper.values = [[[] for j in range(0, Nd)] for i in range(0, Nd)]
+        for row in range(0, Nd):
+            for column in range(0, Nd):
+                for value in range(1, 10):
+                    if ((given.values[row][column] == 0) and not (given.is_column_duplicate(column, value) or given.is_block_duplicate(row, column, value) or given.is_row_duplicate(row, value))):
+                        # Value is available.
+                        helper.values[row][column].append(value)
+                    elif given.values[row][column] != 0:
+                        # Given/known value from file.
+                        helper.values[row][column].append(given.values[row][column])
+                        break
+
+        # Seed a new population.
+        for p in range(0, Nc):
+            g = Individual()
+            for i in range(0, Nd):  # New row in individual.
+                row = np.zeros(Nd)
+
+                # Fill in the givens.
+                for j in range(0, Nd):  # New column j value in row i.
+
+                    # If value is already given, don't change it.
+                    if given.values[i][j] != 0:
+                        row[j] = given.values[i][j]
+                    # Fill in the gaps using the helper board.
+                    elif given.values[i][j] == 0:
+                        row[j] = helper.values[i][j][random.randint(0, len(helper.values[i][j]) - 1)]
+
+                # If we don't have a valid board, then try again. max iteration 500,000
+                # There must be no duplicates in the row.
+                ii = 0
+                while len(list(set(row))) != Nd:
+                    ii += 1
+                    if ii > 500000:
+                        return 0
+                    for j in range(0, Nd):
+                        if given.values[i][j] == 0:
+                            row[j] = helper.values[i][j][random.randint(0, len(helper.values[i][j]) - 1)]
+
+                g.values[i] = row
+            # print(g.values)
+            self.individuals.append(g)
+        # print(self.individuals[0])
+        # Compute the fitness of all individuals in the population.
+        self.update_fitness()
+
+        # print("Seeding complete.")
+
+        return 1
+
+    def update_fitness(self):
+        """ Update fitness of every individual/chromosome. """
+        for individual in self.individuals:
+
+            individual.update_fitness()
+        return
+
+    def sort(self):
+        """ Sort the population based on fitness. """
+        self.individuals = sorted(self.individuals, key=operator.attrgetter('fitness'))
+        return
